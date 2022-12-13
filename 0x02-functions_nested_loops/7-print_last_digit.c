4151bc5e4cd436a18481003e1df14ae6684edc6e
collo324 <emmanuellekishon@1998gmail.com>
2022-12-13 09:37:07 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,10 +7,11 @@
  */
 int print_last_digit(int n)
 {
-	int ld = n % 10;
+	int x;
 
-	if (ld < 0)
-	_putchar(ld + '0');
-
-	return (0);
+	if (n < 0)
+		n = -n;
+	x = % 10;
+	_putchar(x + '0');
+	return (x);
 }
