b039f6c6334c2235c7a6ead1c0c6115cbdb122e8
collo324 <emmanuellekishon@1998gmail.com>
2022-12-19 06:24:39 -0800

    hot fix

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <string.h>
+#include <stdio.h>
 /**
  * print_array - prints n elements of an array of integers
  * @a: point to array
@@ -20,4 +20,5 @@ void print_array(int *a, int n)
 			printf(" ");
 		}
 	}
+	printf("\n");
 }
