9ebc6b0a7e2a167d0872bbe2c06c11b9440f31ce
collo324 <emmanuellekishon@1998gmail.com>
2022-12-21 10:01:36 -0800

    leet

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,5 +3,9 @@
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
+char *leet(char *x);
+char *cap_string(char *);
+char *string_toupper(char *);
+void reverse_array(int *a, int n);
 
 #endif
