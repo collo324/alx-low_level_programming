d684f5b809749f556ca71702eb75cfdb38510619
collo324 <emmanuellekishon@1998gmail.com>
2023-01-03 04:20:46 -0800

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,9 +9,12 @@ void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	for (i = 0; j <= 7; j++)
+	for (i = 0; i <= 7; i++)
 	{
-		_putchar(a[i][j]);
+		for (j = 0; j <= 7; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar(10);
 	}
-	_putchar(10);
 }
