40a84bd8bafb171811f91515b5550ded220be961
collo324 <emmanuellekishon@1998gmail.com>
2023-01-05 09:26:44 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -20,11 +20,11 @@ int _sqrt_recursion(int n)
  */
 int _sqrt(int n, int i)
 {
-	int square = i + i;
+	int square = i * i;
 
 	if (square > n)
 		return (-1);
 	if (square == n)
 		return (i);
-	return (_sqrt(n, i + l));
+	return (_sqrt(n, i + 1));
 }
