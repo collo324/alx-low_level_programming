ad204507a3d6c849738e17e50eef29f777d6efb4
collo324 <emmanuellekishon@1998gmail.com>
2023-01-13 08:06:13 -0800

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <limits.h>
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+
+#endif
