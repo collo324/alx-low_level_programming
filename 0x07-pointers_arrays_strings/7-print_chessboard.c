29e8e068b98e580b35281892a9a4cee961e902de
collo324 <emmanuellekishon@1998gmail.com>
2023-01-23 00:15:41 -0800

    hot fix

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,16 +8,16 @@
 void print_chessboard(char (*a)[8])
 {
 
-int b, c;
+	int b, c;
 
-for (b = 0; b < 8; b++)
-{
+	for (b = 0; b < 8; b++)
+	{
 
-for (c = 0; c < 8; c++)
-{
-_putchar(a[b][c]);
-}
+		for (c = 0; c < 8; c++)
+		{
+			_putchar(a[b][c]);
+		}
 
-_putchar('\n');
-}
+		_putchar('\n');
+	}
 }
